4d6937c4c2f1ba182c5f83b7cab12c78a2bffc54
wus4 <wus4@rose-hulman.edu>
2019-01-31 09:42:04 -0500

    .

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -3,8 +3,8 @@ This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Shixin Wu
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
@@ -13,37 +13,67 @@ from tkinter import ttk
 def main():
     """ Constructs a GUI with stuff on it. """
     # -------------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # done: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # -------------------------------------------------------------------------
+    root=tkinter.Tk()
+    # root.mainloop()
+
 
     # -------------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # done: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # -------------------------------------------------------------------------
+    frame1 = ttk.Frame(root, padding=10)
+    frame1.grid()
 
+    # root.mainloop()
     # -------------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # done: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # -------------------------------------------------------------------------
-
+    go_forward_button = ttk.Button(frame1, text='Forward')
+    go_forward_button.grid()
+    # root.mainloop()
     # -------------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    # done: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # -------------------------------------------------------------------------
+    frame1 = ttk.Frame(root, padding=10)
+    frame1.grid()
 
+    print_stuff_button = ttk.Button(frame1, text='Hello')
+    print_stuff_button['command'] = (lambda:
+                                     print("response"))
+    print_stuff_button.grid()
+
+    # root.mainloop()
     # -------------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # done: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # -------------------------------------------------------------------------
+    frame1 = ttk.Frame(root, padding=10)
+    frame1.grid()
+
+    my_entry_box = ttk.Entry(frame1)
+    my_entry_box.grid()
+
+    print_entry_button = ttk.Button(frame1, text='Print entry')
+    print_entry_button['command'] = (lambda:
+                                     print_contents(my_entry_box))
+    print_entry_button.grid()
+
+
+
+
 
     # -------------------------------------------------------------------------
-    # TODO: 7.
+    # done: 7.
     #    -- Put a second Entry on the Frame.
     #    -- Put a third Button on the frame.
     #    -- Make this new Button respond to a button-press as follows:
@@ -65,12 +95,40 @@ def main():
     #      s = entry_box.get()
     #      n = int(s)
     ####################################################################
+    # frame2 = ttk.Frame(root, padding=10)
+    # frame2.grid()
+    my_entry_box2 = ttk.Entry(frame1)
+    my_entry_box2.grid()
+    x=my_entry_box.get()
+    print_entry_button2 = ttk.Button(frame1, text='Integer Entry')
+    print_entry_button2['command'] = (lambda: print_contents2(my_entry_box2,my_entry_box))
+    print_entry_button2.grid()
+    go_for_button = ttk.Button(frame1, text='For')
+    go_for_button.grid()
 
     # -------------------------------------------------------------------------
-    # TODO: 8. As time permits, do other interesting GUI things!
+    # done: 8. As time permits, do other interesting GUI things!
     # -------------------------------------------------------------------------
 
+    root.mainloop()
+def print_contents(enter):
+    x=enter.get()
+    if x=="ok":
+        print("Hello")
+        # return "Hello"
+    else:
+        print("Goodbye")
+        # return ("Goodbye")
+    # return x
+def print_contents2(enter,enter2):
 
+    y=int(enter.get())
+    if y==int:
+
+        for k in range (y):
+            print_contents(enter2)
+    else:
+        print_contents("error")
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
--- src/m6_grid_row_and_column.py
+++ src/m6_grid_row_and_column.py
@@ -3,8 +3,8 @@ This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Shixin Wu
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
@@ -13,7 +13,7 @@ from tkinter import ttk
 def main():
     """ Constructs a GUI that will be used MUCH later to control EV3. """
     # -------------------------------------------------------------------------
-    # TODO: 2. Follow along with the video to make a remote control GUI
+    # done: 2. Follow along with the video to make a remote control GUI
     # For every grid() method call you will add a row and a column argument
     # -------------------------------------------------------------------------
 
@@ -24,59 +24,59 @@ def main():
     main_frame.grid()  # only grid call that does NOT need a row and column
 
     left_speed_label = ttk.Label(main_frame, text="Left")
-    left_speed_label.grid()
+    left_speed_label.grid(row=1,column=1)
     left_speed_entry = ttk.Entry(main_frame, width=8)
     left_speed_entry.insert(0, "600")
-    left_speed_entry.grid()
+    left_speed_entry.grid(row=2,column=1)
 
     right_speed_label = ttk.Label(main_frame, text="Right")
-    right_speed_label.grid()
+    right_speed_label.grid(row=1,column=3)
     right_speed_entry = ttk.Entry(main_frame, width=8, justify=tkinter.RIGHT)
     right_speed_entry.insert(0, "600")
-    right_speed_entry.grid()
+    right_speed_entry.grid(row=2,column=3)
 
     forward_button = ttk.Button(main_frame, text="Forward")
-    forward_button.grid()
+    forward_button.grid(row=3,column=2)
     forward_button['command'] = lambda: print("Forward button")
     root.bind('<Up>', lambda event: print("Forward key"))
 
     left_button = ttk.Button(main_frame, text="Left")
-    left_button.grid()
+    left_button.grid(row=4,column=1)
     left_button['command'] = lambda: print("Left button")
     root.bind('<Left>', lambda event: print("Left key"))
 
     stop_button = ttk.Button(main_frame, text="Stop")
-    stop_button.grid()
+    stop_button.grid(row=4,column=2)
     stop_button['command'] = lambda: print("Stop button")
     root.bind('<space>', lambda event: print("Stop key"))
 
     right_button = ttk.Button(main_frame, text="Right")
-    right_button.grid()
+    right_button.grid(row=4,column=3)
     right_button['command'] = lambda: print("Right button")
     root.bind('<Right>', lambda event: print("Right key"))
 
     back_button = ttk.Button(main_frame, text="Back")
-    back_button.grid()
+    back_button.grid(row=5,column=2)
     back_button['command'] = lambda: print("Back button")
     root.bind('<Down>', lambda event: print("Back key"))
 
     up_button = ttk.Button(main_frame, text="Up")
-    up_button.grid()
+    up_button.grid(row=6,column=1)
     up_button['command'] = lambda: print("Up button")
     root.bind('<u>', lambda event: print("Up key"))
 
     down_button = ttk.Button(main_frame, text="Down")
-    down_button.grid()
+    down_button.grid(row=7,column=1)
     down_button['command'] = lambda: print("Down button")
     root.bind('<j>', lambda event: print("Down key"))
 
     # Buttons for quit and exit
     q_button = ttk.Button(main_frame, text="Quit")
-    q_button.grid()
+    q_button.grid(row=6,column=3)
     q_button['command'] = lambda: print("Quit button")
 
     e_button = ttk.Button(main_frame, text="Exit")
-    e_button.grid()
+    e_button.grid(row=7,column=3)
     e_button['command'] = lambda: exit()
 
     root.mainloop()
